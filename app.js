0b9eb12807a60799c344ccb48e914d8b7750c99e
amitrathik <amit@themobidev.co>
2015-09-16 10:04:19 -0400

    separated views

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-angular.module('calendarApp',[])
+angular.module('calendarApp',['ngRoute'])
+	.config(function($routeProvider) {
+  var resolveCalendar = {
+    calendarApp: function (calendarApp) {
+      return calendarApp.fetch();
+    }
+  };
+ 
+  $routeProvider
+    .when('/', {
+      controller:'calendarController as calendar',
+      templateUrl:'calendar.html'
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+	})
 	.controller('calendarController', function(){
 		var calendar = this;
 		// List of Events
