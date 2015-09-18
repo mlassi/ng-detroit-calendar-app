79e2dd02d0ce71c59b6d51f30bab12ee40eb93a6
Michael Onslow <mikeonslow@Michaels-MacBook-Pro.local>
2015-09-17 23:02:05 -0400

    Added base calendar mock and styling

--- app.js
+++ app.js
@@ -1,29 +1,42 @@
-angular.module('calendarApp',['ngRoute'])
-	.config(function($routeProvider) {
-  var resolveCalendar = {
-    calendarApp: function (calendarApp) {
-      return calendarApp.fetch();
-    }
-  };
- 
-  $routeProvider
-    .when('/', {
-      controller:'calendarController as calendar',
-      templateUrl:'calendar.html'
+angular.module('calendarApp', ['ngRoute'])
+    .config(function ($routeProvider) {
+        var resolveCalendar = {
+            calendarApp: function (calendarApp) {
+                return calendarApp.fetch();
+            }
+        };
+
+        $routeProvider
+            .when('/monthview/:date', {
+                controller: 'calendarController as calendar',
+                templateUrl: 'calendar.html'
+            })
+            .when('/weekview/:date/:weeknum', {
+                controller: 'calendarController as calendar',
+                templateUrl: 'week.html'
+            })
+            .otherwise({
+                redirectTo: '/monthview/default'
+            });
     })
-    .otherwise({
-      redirectTo:'/'
+    .controller('calendarController', function () {
+
+        // List of Events
+        this.events = [
+            {id: 1, title: 'ng-Detroit event 1', date: '04/20/2015'},
+            {id: 2, title: 'ng-Detroit event 2', date: '06/03/2015'}
+        ];
+        // Add Event
+        this.addEvent = function () {
+            this.events.push({title: calendar.eventTitle, date: calendar.eventDate});
+        };
+
+        this.removeEvent = function (event_id) {
+            for(var i in this.events){
+                if(event_id == this.events[i].id) {
+                    this.events.splice(i, 1);
+                }
+            }
+        }
+
     });
-	})
-	.controller('calendarController', function(){
-		var calendar = this;
-		// List of Events
-		calendar.events = [
-			{id:1,title:'ng-Detroit event 1',date:'04/20/2015'},
-			{id:2,title:'ng-Detroit event 2',date:'06/03/2015'}
-		];
-		// Add Event
-		calendar.addEvent = function(){
-			calendar.events.push({title:calendar.eventTitle,date:calendar.eventDate});
-		};
-	});
