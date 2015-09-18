97dd1e62077966837a6c8605750ac6ee201a6757
Michael Onslow <mikeonslow@Michaels-MacBook-Pro.local>
2015-09-17 20:30:44 -0400

    Added "Calendar" class with some basic date helper functions

--- classes/calendar.js
+++ classes/calendar.js
@@ -1,3 +1,40 @@
-/**
- * Created by mikeonslow on 9/17/15.
- */
+function Calendar(date) {
+
+    this.month_names = new Array("January", "February", "March", "April", "May", "June", "July",
+        "August", "September", "October", "November", "December");
+
+    this.day_names = new Array("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday");
+
+    this.getDaysInMonth = function() {
+        return this.days_in_month;
+    }
+
+    this.setFirstDayofMonth = function() {
+        var d = new Date(this.date);
+        d.setDate(1);
+        this.first_day_of_month = d.getDay();
+    }
+
+    this.getFirstDayOfMonth = function() {
+        return this.first_day_of_month;
+    }
+
+    this.init = function(date) {
+        if(typeof date == 'string') {
+            date = new Date(date);
+        } else if(!(date instanceof Date)) {
+            date = new Date();
+        }
+
+        this.date = date;
+        this.year = date.getFullYear();
+        this.month = date.getMonth() + 1;
+        this.day = date.getDate();
+        this.days_in_month = new Date(this.year, this.month, 0).getDate();
+        this.setFirstDayofMonth();
+    }
+
+    this.init(date);
+
+}
+
